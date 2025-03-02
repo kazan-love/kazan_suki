fc277aad21eb4c1cc9291b447172e1c8755bae7f
kazan.love <sakurazima.kazan@gmail.com>
2025-03-03 01:32:37 +0900

    Updated HTML, CSS, and JavaScript files to display text only

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // script.js
 document.addEventListener('DOMContentLoaded', function () {
     const heroText = document.querySelector('.hero-text');
-    const overlay = document.querySelector('.overlay');
 
     // 初回表示時のアニメーション
     window.addEventListener('scroll', function () {
@@ -9,10 +8,6 @@ document.addEventListener('DOMContentLoaded', function () {
         if (scrollPosition > 100) {
             heroText.style.opacity = '1';
             heroText.classList.add('permanent');
-        }
-        if (scrollPosition > 300) {
-            overlay.style.opacity = '1';
-            overlay.style.transform = 'translateX(0)';
         }
     });
 
