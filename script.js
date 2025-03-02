173648c693af23a6d619637934b47544d5398fb3
kazan.love <sakurazima.kazan@gmail.com>
2025-03-02 23:37:57 +0900

    Updated content and added new files

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 window.addEventListener('scroll', function() {
-    var content = document.querySelector('.content');
-    var contentPosition = content.getBoundingClientRect().top;
-    var screenPosition = window.innerHeight / 1.5;
+    var contents = document.querySelectorAll('.text');
+    contents.forEach(function(content) {
+        var contentPosition = content.getBoundingClientRect().top;
+        var screenPosition = window.innerHeight / 1.3;
 
-    if (contentPosition < screenPosition) {
-        content.classList.add('scroll-active');
-    }
+        if (contentPosition < screenPosition) {
+            content.classList.add('scroll-active');
+        }
+    });
 });
